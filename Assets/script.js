279182a72cf14d2f3d8643b455b6e5995b7f2b1f
Ash <ashloraptor@gmail.com>
2023-12-29 10:27:43 -0500

    Added framework for functions renderFavoritesHistory and renderGroceryList in quotes.

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,36 @@ var searchForm = document.getElementById("search-form");
 var searchInput = document.getElementById("search-input");
 //var apiUrl = 
 
+//function searchRecipes(){}
+
+/*function renderFavoritesHistory(){
+
+//card elements
+var col = document.createElement('div');
+var card = document.createElement('div');
+var cardBody = document.createElement('div');
+var cardTitle = document.createElement('h5');
+
+//incorporate cards
+col.append(card);
+card.append(cardBody);
+cardBody.append(cardTitle);
+}*/
+
+/*function renderGroceryList(){
+
+//card elements
+var col = document.createElement('div');
+var card = document.createElement('div');
+var cardBody = document.createElement('div');
+var cardTitle = document.createElement('h5');
+
+//incorporate cards
+col.append(card);
+card.append(cardBody);
+cardBody.append(cardTitle);
+}*/
+
 //Form function
 function logMessage(event) {
     // Prevent the default form submission
@@ -11,28 +41,28 @@ function logMessage(event) {
     var inputValue = searchInput.value.trim();
     // Log the message to the console
     console.log("You searched for: " + inputValue);
-  
+
     searchInput.value = '';
     //callback to search function (the one that does the fetch request). Include parameter "input value"
 
-  }
+}
 
-  function fetchRecipes(){
+function fetchRecipes() {
     //APIKey
     //var apiURL = 
 
     fetch(apiUrl)
-    .then(function(res){
-        return res.json();
-    })
-    .then(function (data) {
-        renderItems();
-    })
-    .catch(function (err) {
-        console.error(err);
-    })
-  }
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            renderItems();
+        })
+        .catch(function (err) {
+            console.error(err);
+        })
+}
 
-  //initFavoritesHistory
-  //initGroceryList
-  searchForm.addEventListener("submit", logMessage);
+//initFavoritesHistory
+//initGroceryList
+searchForm.addEventListener("submit", logMessage);
